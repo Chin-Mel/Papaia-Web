c50157714d44855ffbdd482808df96034f5ad1cb
Chin-Mel <francineduarte523@gmail.com>
2025-07-10 21:17:04 +0800

    Initial Commit - Papaia Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing">
+      <header className="landing-header">
+        <h1>Papaia</h1>
+        <p>Your smart papaya health detection companion 🍃</p>
+        <button>Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
