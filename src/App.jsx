3ed5d4da8a4af3507f1d3cf4b51de5629004e880
Chin-Mel <francineduarte523@gmail.com>
2025-07-29 07:06:34 +0800

    tarong

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 // App.jsx
+import { Routes, Route } from "react-router-dom";
+import Welcome from "./pages/WelcomePage/Welcome";
+import Login from "./pages/LoginPage/Login";
+import Register from "./pages/RegisterPage/Register";
+import ForgotPassword from "./pages/ForgotPasswordPage/ForgotPassword";
+import Verification from "./pages/VerificationPage/Verification";
+import NewPassword from "./pages/NewPasswordPage/NewPassword";
 import Dashboard from "./pages/DashboardPage/Dashboard";
+import AboutPage from "./pages/AboutPage/About";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/verify-otp" element={<Verification />} />
+      <Route path="/new-password" element={<NewPassword />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Routes>
+  );
 }
 
 export default App;
