efb6e815f4757eceacb86a418e23e6015dc05df9
Chin-Mel <francineduarte523@gmail.com>
2025-08-15 22:51:22 +0800

    gana na ba huhuhuuhuhhuhu

--- src/pages/NewPasswordPage.jsx
+++ src/pages/NewPasswordPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import HeaderStart from "../../components/Header/HeaderStart";
+import HeaderStart from "../components/Header/HeaderStart";
 import { FaSignInAlt } from "react-icons/fa";
 
 export default function NewPasswordPage() {
