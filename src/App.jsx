0c1a6e0480b58e3891b5a1792d66a56f89fbf107
Chin-Mel <francineduarte523@gmail.com>
2025-08-16 16:31:39 +0800

    tarong na

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,18 @@
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import LandingPage from "./pages/LandingPage.jsx";
-import Login from "./pages/LoginPage/Login.jsx";
-import Register from "./pages/RegisterPage/Register.jsx";
+import SignIn from "./pages/SignInPage.jsx";
+import SignUp from "./pages/SignUpPage.jsx";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
 import OtpVerificationPage from "./pages/OtpVerificationPage.jsx";
 import NewPasswordPage from "./pages/NewPasswordPage.jsx";
-import Dashboard from "./pages/DashboardPage/Dashboard.jsx";
-import FarmDashboard from "./pages/FarmDashboardPage/FarmDashboard.jsx";
-import Profile from "./pages/ProfilePage/Profile.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/sign-in" element={<Login />} />
-      <Route path="/sign-up" element={<Register />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
       <Route path="/otp-verification" element={<OtpVerificationPage />} />
       <Route path="/new-password" element={<NewPasswordPage />} />
