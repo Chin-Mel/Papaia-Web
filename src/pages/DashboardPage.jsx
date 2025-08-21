225503b2e76c9827ba2869f7ecb41aa1d0974abd
Chin-Mel <francineduarte523@gmail.com>
2025-08-21 18:07:55 +0800

    balik na ba

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { Plus, Users, Leaf, ScanLine, TrendingUp, MapPin } from "lucide-react";
 import HeaderMain from "../components/Header/HeaderMain";
 import Footer from "../components/Footer/FooterMain";
