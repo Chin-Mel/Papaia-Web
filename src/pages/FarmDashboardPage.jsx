89656657c369294b9b404ddf07fab5314bdff793
Chin-Mel <francineduarte523@gmail.com>
2025-08-25 13:55:12 +0800

    tarung na

--- src/pages/FarmDashboardPage.jsx
+++ src/pages/FarmDashboardPage.jsx
@@ -17,11 +17,11 @@ import {
 } from "lucide-react";
 import HeaderMain from "../components/Header/HeaderMain";
 import Footer from "../components/Footer/FooterMain";
-import AddFarmerModal from "./Popups/AddFarmerModal";
-import FarmerDetailModal from "./Popups/FarmerDetailModal";
-import RemoveFarmerModal from "./Popups/RemoveFarmerModal";
-import FarmerAddedSuccessModal from "./Popups/FarmerAddedSuccessModal";
-import FarmerRemovedSuccessModal from "./Popups/FarmerRemovedSuccessModal";
+import AddFarmerModal from "../Popups/AddFarmerModal";
+import FarmerDetailModal from "../Popups/FarmerDetailModal";
+import RemoveFarmerModal from "../Popups/RemoveFarmerModal";
+import FarmerAddedSuccessModal from "../Popups/FarmerAddedSuccessModal";
+import FarmerRemovedSuccessModal from "../Popups/FarmerRemovedSuccessModal";
 
 export default function FarmDashboardPage() {
   const [timeFilter, setTimeFilter] = useState("Daily");
