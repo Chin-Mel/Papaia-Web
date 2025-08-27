0cdc62e5ab3214990b68d17f5b056d4008d2ec75
Chin-Mel <francineduarte523@gmail.com>
2025-08-27 17:09:52 +0800

    inatay

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,10 +1,26 @@
+// ProtectedRoute.jsx
+import React from "react";
 import { Navigate } from "react-router-dom";
+import jwt_decode from "jwt-decode";
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token"); // check for JWT
 
   if (!token) {
     return <Navigate to="/" replace />; // redirect if not logged in
+  }
+
+  try {
+    const { exp } = jwt_decode(token);
+    if (Date.now() >= exp * 1000) {
+      // Token expired
+      localStorage.removeItem("token");
+      return <Navigate to="/sign-in" replace />;
+    }
+  } catch (err) {
+    // Invalid token
+    localStorage.removeItem("token");
+    return <Navigate to="/" replace />;
   }
 
   return children; // render protected page
