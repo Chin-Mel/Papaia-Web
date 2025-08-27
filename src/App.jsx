6011eab6c5316aa692bd0399c1cebdded16149c3
Chin-Mel <francineduarte523@gmail.com>
2025-08-27 12:15:12 +0800

    authcontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./AuthContext";
 import ProtectedRoute from "./ProtectedRoute";
 
 import LandingPage from "./pages/LandingPage";
