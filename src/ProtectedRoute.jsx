f7fc354a52b57ae91ea904ece58686f7b09106c7
Chin-Mel <francineduarte523@gmail.com>
2025-08-27 12:11:09 +0800

    gana na please

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 // src/components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = () => {
   const { user, isLoading } = useAuth();
