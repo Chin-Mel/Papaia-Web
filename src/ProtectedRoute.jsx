271d31141e11874ac9ea5a4f950b3fea888795e8
Chin-Mel <francineduarte523@gmail.com>
2025-08-27 13:24:20 +0800

    mana

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -14,7 +14,7 @@ const ProtectedRoute = ({ children }) => {
 
   // Redirect to login if not authenticated
   if (!isAuthenticated) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
+    return <Navigate to="/" state={{ from: location }} replace />;
   }
 
   return children;
