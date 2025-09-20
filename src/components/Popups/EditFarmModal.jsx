aa3b2e0952ab7d44abde0a87a3b6bbb1cfb15342
Chin-Mel <francineduarte523@gmail.com>
2025-09-20 17:11:32 +0800

    edit farm data

--- src/components/Popups/EditFarmModal.jsx
+++ src/components/Popups/EditFarmModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { X, Leaf, Save } from "lucide-react";
 
-function EditFarmModal({ isOpen, onClose, formData }) {
+function EditFarmModal({ isOpen, onClose, farmData }) {
   const [formData, setFormData] = useState({
     farmName: "",
     location: "",
