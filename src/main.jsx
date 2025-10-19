2a74ccc5cad49c606e8ee5672e6c9dbc6d039a8b
Chin-Mel <francineduarte523@gmail.com>
2025-10-19 13:16:13 +0800

    console logs erasing

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.warn = () => {};
+  console.error = () => {};
+  console.info = () => {};
+  console.debug = () => {};
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
