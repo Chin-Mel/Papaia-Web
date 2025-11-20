46443533f8b109bde3d55141791116d6d843c8b3
Chin-Mel <francineduarte523@gmail.com>
2025-11-20 16:04:17 +0800

    SignUp Enhancement 14

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -453,8 +453,8 @@ export default function SignUpPage() {
                 </div>
 
                 {/* Account Information Column */}
-                <div>
-                  <div className="lg:pl-8">
+                <div className="lg:pl-8">
+                  <div className="flex items-center gap-3 mb-6">
                     <div className="w-10 h-10 rounded-xl bg-gradient-to-br from-orange-500 to-orange-600 flex items-center justify-center">
                       <Lock className="w-5 h-5 text-white" />
                     </div>
