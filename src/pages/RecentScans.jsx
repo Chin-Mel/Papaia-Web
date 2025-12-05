294312ed2e27fdfe6c10ec63f155b6d3d947776b
Chin-Mel <francineduarte523@gmail.com>
2025-12-05 12:54:39 +0800

    heightchanged

--- src/pages/RecentScans.jsx
+++ src/pages/RecentScans.jsx
@@ -468,7 +468,7 @@ export default function RecentScans({ farmId, timeFilter, dateRange }) {
               <div className="border-b border-gray-200 style={{ paddingTop: 0, paddingBottom: 0 }}">
                 {chartData.length > 0 ? (
                   <>
-                    <ResponsiveContainer width="100%" height={300}>
+                    <ResponsiveContainer width="100%" height={370}>
                       <PieChart>
                         <Pie
                           data={chartData}
