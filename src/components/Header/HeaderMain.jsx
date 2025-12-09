3397057b6715da91d9f8565a30e57dd452b5c757
Chin-Mel <francineduarte523@gmail.com>
2025-12-09 15:10:46 +0800

    logo resize

--- src/components/Header/HeaderMain.jsx
+++ src/components/Header/HeaderMain.jsx
@@ -4,7 +4,7 @@ import { ChevronDown, LogOut, Bell } from "lucide-react";
 import { getLoggedInUser } from "../../utils/security";
 import { useNotifications } from "../../NotificationContext";
 
-import papaiaLogo from "../../assets/ic_papaia_logo_no_word.png";
+import PapaiaLogo from "../../assets/ic_papaia_logo_no_word.png";
 import hamburgerMenuIcon from "../../assets/burger-bar.png";
 import defaultUser from "../../assets/default-user.png";
 
